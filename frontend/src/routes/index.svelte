b6c214833c3e977a83f985d557b13ddbc5c975fb
nikolay.ilnitsky <nikolay.ilnitsky@x5.ru>
2025-11-27 00:20:52 +0300

    render board error

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -2,14 +2,22 @@
     import {Board} from "$lib/components";
     import {useBoard} from "$api/board";
     import {Spinner} from "$lib/ui/spinner";
-    const board = useBoard(1)
+
+    const board = useBoard(1);
 </script>
+
 <div class="flex flex-col gap-5 flex-1">
     {#if board.isPending}
         <div class="flex flex-col items-center justify-center h-full w-full">
-            <Spinner class="size-15"/>
+            <Spinner class="size-15" />
         </div>
-        {:else if board.data}
+    {:else if board.error}
+        <div class="flex flex-col items-center justify-center h-full w-full">
+            <h1 class="text-4xl font-bold">
+                {board.error.message}
+            </h1>
+        </div>
+    {:else if board.data}
         <h1 class="font-semibold text-2xl">{board.data.title}</h1>
         <Board columnItems={board.data.columns}/>
     {/if}
